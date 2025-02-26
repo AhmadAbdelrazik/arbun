8d8af0c9f082c11d295aa9945d924623e8d5abba
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2025-02-27 01:28:16 +0200

    Removed admin and customer repositories

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,23 +1,19 @@
 package models
 
 type Model struct {
-	Products  *ProductModel
-	Admins    *AdminModel
-	Customers *CustomerModel
-	Tokens    *TokenModel
-	Carts     *CartModel
-	Orders    *OrderModel
-	Users     *UserModel
+	Products *ProductModel
+	Tokens   *TokenModel
+	Carts    *CartModel
+	Orders   *OrderModel
+	Users    *UserModel
 }
 
 func NewModel() *Model {
 	return &Model{
-		Products:  newProductModel(),
-		Admins:    newAdminModel(),
-		Customers: newCustomerModel(),
-		Tokens:    newTokenModel(),
-		Carts:     newCartModel(),
-		Orders:    newOrderModel(),
-		Users:     newUserModel(),
+		Products: newProductModel(),
+		Tokens:   newTokenModel(),
+		Carts:    newCartModel(),
+		Orders:   newOrderModel(),
+		Users:    newUserModel(),
 	}
 }
